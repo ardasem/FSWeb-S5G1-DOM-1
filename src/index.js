583e18ba3e4f27260abe617848d5a785552830a4
ardasem <73624089+ardasem@users.noreply.github.com>
2023-09-21 04:46:03 +0300

    homework finished

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
-  "nav": {
+const siteContent = {
+  // BU NESNEYİ DEĞİŞTİRMEYİN
+  nav: {
     "nav-item-1": "Servisler",
     "nav-item-2": "Ürünler",
     "nav-item-3": "Vizyon",
@@ -7,30 +8,35 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
     "nav-item-5": "Hakkımızda",
     "nav-item-6": "İletişim",
   },
-  "cta": {
-    "h1": "Bu DOM Mükemmel",
-    "button": "Başlayın",
+  cta: {
+    h1: "Bu DOM Mükemmel",
+    button: "Başlayın",
   },
   "ana-içerik": {
     "özellikler-h4": "Özellikler",
-    "özellikler-içerik": "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "özellikler-içerik":
+      "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "hakkımızda-h4": "Hakkında",
-    "hakkımızda-içerik": "Hakkında içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "hakkımızda-içerik":
+      "Hakkında içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "servisler-h4": "Servisler",
-    "servisler-içeriği": "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "servisler-içeriği":
+      "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "ürünler-h4": "Ürünler",
-    "ürünler-içeriği": "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "ürünler-içeriği":
+      "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vizyon-h4": "Vizyon",
-    "vizyon-içeriği": "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vizyon-içeriği":
+      "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "iletisim": {
+  iletisim: {
     "iletişim-h4": "İletişim",
-    "adres": "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
-    "telefon": "+90 (123) 456-7899",
-    "email": "satis@birsirketsitesi.com.tr",
+    adres: "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
+    telefon: "+90 (123) 456-7899",
+    email: "satis@birsirketsitesi.com.tr",
   },
-  "footer": {
-    "copyright": "Copyright Bir Şirket Sitesi 2022",
+  footer: {
+    copyright: "Copyright Bir Şirket Sitesi 2022",
   },
   "images": {
     "logo-img": "http://localhost:9000/img/logo.png",
@@ -39,7 +45,98 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
   },
 };
 
-console.log('Proje açıldı!')
+console.log("Proje açıldı!");
+
+const anchorElements = document.querySelectorAll("a");
+
+anchorElements.forEach((anchor, index) => {
+  anchor.textContent = siteContent.nav[`nav-item-${index + 1}`];
+  anchor.setAttribute('class','italic')
+});
+
+document
+  .querySelector("#logo-img")
+  .setAttribute("src", siteContent.images["logo-img"]);
+
+document.querySelector(".cta-text h1").textContent = siteContent.cta.h1;
+document.querySelector(".cta-text button").textContent = siteContent.cta.button;
+
+document
+  .querySelector("#cta-img")
+  .setAttribute("src", siteContent.images["cta-img"]);
+
+const topContent = document.querySelector(".top-content");
+const topContentHeadingElements = topContent.querySelectorAll(".text-content h4");
+const topContentParagraphElements = topContent.querySelectorAll(".text-content p");
+
+topContentHeadingElements.forEach((heading, index) => {
+  const key = index === 0 ? siteContent["ana-içerik"]["özellikler-h4"] : siteContent["ana-içerik"]["hakkımızda-h4"];
+
+  heading.textContent = key;
+});
+
+topContentParagraphElements.forEach((paragraph, index) => {
+  const key = index === 0 ? siteContent["ana-içerik"]["özellikler-içerik"] : siteContent["ana-içerik"]["hakkımızda-içerik"];
+
+  paragraph.textContent = key;
+});
 
 
-/* Kodlar Buradan aşağıya */
+document
+  .querySelector("#middle-img")
+  .setAttribute("src", siteContent.images["accent-img"]);
+
+const bottomContent = document.querySelector(".bottom-content");
+const bottomContentHeadingElements = bottomContent.querySelectorAll(".text-content h4");
+const bottomContentParagraphElements = bottomContent.querySelectorAll(".text-content p");
+
+bottomContentHeadingElements.forEach((heading, index) => {
+  let text = '';
+
+  if(index === 0){
+    text = siteContent["ana-içerik"]["servisler-h4"]
+  } else if (index === 1){
+    text = siteContent["ana-içerik"]["ürünler-h4"]
+  } else {
+    text = siteContent["ana-içerik"]["vizyon-h4"]
+  }
+
+  heading.textContent = text;
+});
+
+bottomContentParagraphElements.forEach((paragraph, index) => {
+  let text = '';
+
+  if(index === 0){
+    text = siteContent["ana-içerik"]["servisler-içeriği"]
+  } else if (index === 1){
+    text = siteContent["ana-içerik"]["ürünler-içeriği"]
+  } else {
+    text = siteContent["ana-içerik"]["vizyon-içeriği"]
+  }
+
+  paragraph.textContent = text; 
+});
+
+document.querySelector('.contact h4').textContent = siteContent.iletisim["iletişim-h4"]
+
+const contactParagraphElements = document.querySelectorAll('.contact p')
+
+contactParagraphElements.forEach((paragraph, index) => {
+  let text = '';
+
+  if(index === 0){
+    text = siteContent.iletisim.adres
+  } else if (index === 1){
+    text = siteContent.iletisim.telefon
+  } else {
+    text = siteContent.iletisim.email
+  }
+
+  paragraph.textContent = text; 
+});
+
+
+
+document.querySelector('footer a').setAttribute('class','bold')
+document.querySelector('footer a').textContent= siteContent.footer.copyright
